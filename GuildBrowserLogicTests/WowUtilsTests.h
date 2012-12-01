05752d9aefcf7e9eb34c45d1e6d7819504869a42
Sergi Kuhs <sergi.kuhs@gmail.com>
2012-12-01 09:47:14 +0100

    first test implemented

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Sergio Kuhs on 01.12.12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
